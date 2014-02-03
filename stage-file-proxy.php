15c6566b9304c37af367afef3ffca2ced1861309
Austin Smith <austin@alleyinteractive.com>
2014-02-03 08:57:44 -0500

    add dynamic resize capability

--- stage-file-proxy.php
+++ stage-file-proxy.php
@@ -17,6 +17,9 @@
  *
  * Developers need to know that this stuff is happening and should generally understand how this plugin 
  * works before they employ it.
+ *
+ * The dynamic resizing portion was adapted from dynamic-image-resizer.
+ * See: http://wordpress.org/plugins/dynamic-image-resizer/
  */
 
 /**
@@ -45,47 +48,128 @@ function sfp_expect() {
 	add_action( 'init', 'sfp_dispatch' );
 }
 
+/**
+ * This function can fetch a remote image or resize a local one.
+ *
+ * If a cropped image is requested, and the original does not exist locally, it will take two runs of
+ * this function to return the proper resized image, which is achieved by the header("Location: ...")
+ * bits. The first run will fetch the remote image, the second will resize it.
+ *
+ * Ideally we could do this in one pass.
+ */
 function sfp_dispatch() {
 	$mode = sfp_get_mode();
-	$remote_url = sfp_get_remote_url();
+	$relative_path = sfp_get_relative_path();
+
 	if ( 'header' === $mode ) {
-		header( "Location: $remote_url" );
+		header( "Location: " . sfp_get_base_url() . $relative_path );
+		exit;
+	}
+
+	// resize an image maybe
+	if ( preg_match( '/(.+)(-r)?-([0-9]+)x([0-9]+)(c)?\.(jpe?g|png|gif)/iU', $relative_path, $matches ) ) {
+		$resize = array();
+		$resize['filename'] = $matches[1].'.'.$matches[6];
+		$resize['width'] = $matches[3];
+		$resize['height'] = $matches[4];
+		$resize['crop'] = !empty( $matches[5] );
+		$resize['mode'] = substr( $matches[2], 1 );
+		$uploads_dir = wp_upload_dir();
+
+		$basefile = $uploads_dir['basedir'] . '/' . $resize['filename'];
+	
+		if ( file_exists( $basefile ) ) {
+			$suffix = $resize['width'] . 'x' . $resize['height'];
+			if ( $resize['crop'] ) $suffix .= 'c';
+			if ( 'r' == $resize['mode'] ) $suffix = 'r-' . $suffix;
+			$img = wp_get_image_editor( $basefile );
+			$img->resize( $resize['width'], $resize['height'], $resize['crop'] );
+			$info = pathinfo( $basefile );
+			$img->save( $info['dirname'] . '/' . $info['filename'] . '-' . $suffix . '.' .$info['extension'] );
+			header( "Location: {$_SERVER['REQUEST_URI']}" );
+		}
+		$relative_path = $resize['filename'];
+	}
+
+	// Download a full-size original from the remote server.
+	// If it needs to be resized, it will be on the next load.
+	$remote_url = sfp_get_base_url() . $relative_path;
+	
+	$remote_request = wp_remote_get( $remote_url, array( 'timeout' => 30 ) );
+
+	if ( is_wp_error( $remote_request ) || $remote_request['response']['code'] > 400 ) {
+		sfp_error();
+	}
+
+	// we could be making some dangerous assumptions here, but if WP is setup normally, this will work:
+	$path_parts = explode( '/', $remote_url );
+	$name = array_pop( $path_parts );
+
+	if ( strpos( $name, '?' ) ) list( $name, $crap ) = explode( '?', $name, 2 );
+
+	$month = array_pop( $path_parts );
+	$year = array_pop( $path_parts );
+
+	$upload = wp_upload_bits( $name, null, $remote_request['body'], "$year/$month" );
+
+	if ( !$upload['error'] ) {
+		// if there was some other sort of error, and the file now does not exist, we could loop on accident.
+		// should think about some other strategies.
+		header( "Location: {$_SERVER['REQUEST_URI']}" );
 		exit;
 	} else {
-		$remote_request = wp_remote_get( $remote_url, array( 'timeout' => 30 ) );
-
-		if ( is_wp_error( $remote_request ) || $remote_request['response']['code'] > 400 ) {
-			sfp_error();
-		}
-
-		// we could be making some dangerous assumptions here, but if WP is setup normally, this will work:
-		$path_parts = explode( '/', $remote_url );
-		$name = array_pop( $path_parts );
-
-		if ( strpos( $name, '?' ) ) list( $name, $crap ) = explode( '?', $name, 2 );
-
-		$month = array_pop( $path_parts );
-		$year = array_pop( $path_parts );
-
-		$upload = wp_upload_bits( $name, null, $remote_request['body'], "$year/$month" );
-
-		if ( !$upload['error'] ) {
-			// if there WAS an error, and the file now does not exist, we could churn on accident.
-			// should think about some other strategies.
-			header( "Location: {$_SERVER['REQUEST_URI']}" );
-			exit;
-		} else {
-			sfp_error();
-		}
+		sfp_error();
 	}
 }
 
 /**
- * Build the full URL to the remote path.
+ * prevent WP from generating resized images on upload
  */
-function sfp_get_remote_url() {
-	return sfp_get_base_url() . sfp_get_relative_path();
+function sfp_image_sizes_advanced( $sizes ) {
+	global $dynimg_image_sizes;
+	
+	// save the sizes to a global, because the next function needs them to lie to WP about what sizes were generated
+	$dynimg_image_sizes = $sizes;
+
+	// force WP to not make sizes by telling it there's no sizes to make
+	return array();
 }
+add_filter( 'intermediate_image_sizes_advanced', 'sfp_image_sizes_advanced' );
+
+/**
+ * Trick WP into thinking the images were generated anyways.
+ */
+function sfp_generate_metadata( $meta ) {
+	global $dynimg_image_sizes;
+	
+	if ( !is_array( $dynimg_image_sizes ) ) return;
+
+	foreach ($dynimg_image_sizes as $sizename => $size) {
+		// figure out what size WP would make this:
+		$newsize = image_resize_dimensions( $meta['width'], $meta['height'], $size['width'], $size['height'], $size['crop'] );
+
+		if ($newsize) {
+			$info = pathinfo( $meta['file'] );
+			$ext = $info['extension'];
+			$name = wp_basename( $meta['file'], ".$ext" );
+
+			$suffix = "r-{$newsize[4]}x{$newsize[5]}";
+			if ( $size['crop'] ) $suffix .='c';
+
+			// build the fake meta entry for the size in question
+			$resized = array(
+				'file' => "{$name}-{$suffix}.{$ext}",
+				'width' => $newsize[4],
+				'height' => $newsize[5],
+			);
+
+			$meta['sizes'][$sizename] = $resized;
+		}
+	}
+	
+	return $meta;
+}
+add_filter( 'wp_generate_attachment_metadata', 'sfp_generate_metadata' );
 
 /**
  * Get the relative file path by stripping out the /wp-content/uploads/ business.
