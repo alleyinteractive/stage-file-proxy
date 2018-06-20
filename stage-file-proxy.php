de3dc4cb7191a6ed47af217e48d606d2a8c62afd
Jake Foster <jake@alley.co>
2018-06-20 01:28:41 -0400

    use option instead of filter

--- stage-file-proxy.php
+++ stage-file-proxy.php
@@ -38,10 +38,7 @@ function sfp_first() {
 }
 add_action( 'activated_plugin', 'sfp_first' );
 
-// @todo is 'init' early enough?
-add_action( 'init', function() {
-	if ( stripos( $_SERVER['REQUEST_URI'], apply_filters( 'sfp_expect_request_uri', '/wp-content/uploads/' ) ) !== false ) sfp_expect();
-} );
+if ( stripos( $_SERVER['REQUEST_URI'], get_option( 'sfp_uri_replace', '/wp-content/uploads/' ) ) !== false ) sfp_expect();
 
 /**
  * This function, triggered above, sets the chain in motion.
